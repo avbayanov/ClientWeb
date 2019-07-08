b16e7c8daaceb0e9b8468b8b00676e9f445015e0
Alexander Bayanov <alex@bayanov.org>
2019-07-08 20:03:15 +0700

    Wrapped code with self invoking function

--- Temperature/converter.js
+++ Temperature/converter.js
@@ -1,49 +1,51 @@
-var celsiusInput;
-var nanMessage;
-var kelvinText;
-var fahrenheitText;
+(function () {
+    var celsiusInput;
+    var nanMessage;
+    var kelvinText;
+    var fahrenheitText;
 
-document.addEventListener("DOMContentLoaded", function () {
-    celsiusInput = document.getElementById("celsius-input");
-    nanMessage = document.getElementById("nan-message");
-    kelvinText = document.getElementById("kelvin-text");
-    fahrenheitText = document.getElementById("fahrenheit-text");
+    document.addEventListener("DOMContentLoaded", function () {
+        celsiusInput = document.getElementById("celsius-input");
+        nanMessage = document.getElementById("nan-message");
+        kelvinText = document.getElementById("kelvin-text");
+        fahrenheitText = document.getElementById("fahrenheit-text");
 
-    var convertButton = document.getElementById("convert-button");
-    convertButton.addEventListener("click", convertButtonClickHandler);
+        var convertButton = document.getElementById("convert-button");
+        convertButton.addEventListener("click", convertButtonClickHandler);
 
-    celsiusInput.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            convertButtonClickHandler();
-        }
-    })
-});
+        celsiusInput.addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
+                convertButtonClickHandler();
+            }
+        })
+    });
 
-function convertToKelvin(celsius){
-    return celsius - 273.15;
-}
-
-function convertToFahrenheit(celsius) {
-    return celsius * (9 / 5) + 32;
-}
-
-function updateKelvinText(kelvin) {
-    kelvinText.innerText = kelvin;
-}
-
-function updateFahrenheitText(fahrenheit) {
-    fahrenheitText.innerText = fahrenheit;
-}
-
-function convertButtonClickHandler() {
-    var toKelvin = convertToKelvin(celsiusInput.value);
-
-    if (isNaN(toKelvin)) {
-        nanMessage.style.display = "inline";
-        return;
+    function convertToKelvin(celsius){
+        return celsius - 273.15;
     }
-    nanMessage.style.display = "none";
 
-    updateKelvinText(toKelvin);
-    updateFahrenheitText(convertToFahrenheit(celsiusInput.value));
-}
+    function convertToFahrenheit(celsius) {
+        return celsius * (9 / 5) + 32;
+    }
+
+    function updateKelvinText(kelvin) {
+        kelvinText.innerText = kelvin;
+    }
+
+    function updateFahrenheitText(fahrenheit) {
+        fahrenheitText.innerText = fahrenheit;
+    }
+
+    function convertButtonClickHandler() {
+        var toKelvin = convertToKelvin(celsiusInput.value);
+
+        if (isNaN(toKelvin)) {
+            nanMessage.style.display = "inline";
+            return;
+        }
+        nanMessage.style.display = "none";
+
+        updateKelvinText(toKelvin);
+        updateFahrenheitText(convertToFahrenheit(celsiusInput.value));
+    }
+})();
