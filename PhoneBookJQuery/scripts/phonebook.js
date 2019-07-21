8b170e1562e452270c88b4240e2333d2cb5e5888
Alexander Bayanov <alex@bayanov.org>
2019-07-21 15:38:31 +0700

    Added href attribute for delete button

--- PhoneBookJQuery/scripts/phonebook.js
+++ PhoneBookJQuery/scripts/phonebook.js
@@ -69,6 +69,7 @@
 
         $("<a>")
             .html("&#10060;")
+            .attr("href", "javascript://")
             .click(function () {
                 showDeleteConfirmation(row);
             })
