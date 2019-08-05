0ab9d83830623eb933a016b3aff962aa7b2008b8
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-08-05 20:34:35 +0700

    Made TodoListVue

---
+++ TodoListVue/scripts/todolistvue.js
@@ -0,0 +1,116 @@
+(function () {
+    new Vue({
+        el: "#app",
+        data: {
+            addNoteText: "",
+            isInvalidInput: false,
+            notes: [],
+            removeTarget: {
+                isExist: false,
+                target: 0
+            }
+        },
+        methods: {
+            addNote: function () {
+                if (this.addNoteText === "") {
+                    this.isInvalidInput = true;
+                    return;
+                }
+                this.isInvalidInput = false;
+
+                this.notes.push(this.addNoteText);
+                this.addNoteText = "";
+            },
+            setRemoveTarget: function (index) {
+                this.removeTarget.isExist = true;
+                this.removeTarget.target = index;
+            },
+            removeNote: function () {
+                this.notes.splice(this.removeTarget, 1);
+            },
+            editNote: function (index, newContent) {
+                Vue.set(this.notes, index, newContent);
+            }
+        }
+    });
+
+    Vue.component("note", {
+        props: ["noteContent", "index"],
+        data: function() {
+            return {
+                isEditing: false,
+                contentHeight: null
+            }
+        },
+        template: "#note-template",
+        methods: {
+            removeSelf: function () {
+                this.$emit("remove-note");
+            },
+            editSelf: function (newContent) {
+                this.$emit("edit-note", this.index, newContent);
+            },
+            updateContentHeight: function () {
+                this.contentHeight = $("p.note-text:contains(" + this.noteContent +")").height();
+            }
+        },
+        mounted: function () {
+            this.updateContentHeight();
+        },
+        updated: function () {
+            this.updateContentHeight();
+        }
+    });
+
+    Vue.component("note-edit", {
+        props: ["initialContent", "fieldHeight"],
+        data: function() {
+            return {
+                editContent: this.initialContent,
+                isInvalid: false
+            }
+        },
+        template: "#edit-note-template",
+        methods: {
+            save: function () {
+                if (this.editContent === "") {
+                    this.isInvalid = true;
+                    return;
+                }
+
+                if (this.editContent !== this.initialContent) {
+                    this.$emit("edit-made", this.editContent);
+                }
+                this.$emit("edit-finished");
+            },
+            cancel: function () {
+                this.$emit("edit-finished");
+            }
+        }
+    });
+
+    Vue.component("remove-modal", {
+        props: ["target"],
+        template: "#remove-modal-template",
+        methods: {
+            showModal: function () {
+                $("#delete-note-modal").modal("show");
+            },
+            hideModal: function () {
+                $("#delete-note-modal").modal("hide");
+            },
+            cancelRemove: function () {
+                this.hideModal();
+                this.$emit("clear-remove-target");
+            },
+            doRemove: function () {
+                this.hideModal();
+                this.$emit("remove-note", this.target);
+                this.$emit("clear-remove-target");
+            }
+        },
+        mounted: function () {
+            this.showModal();
+        }
+    });
+})();
